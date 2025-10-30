a2505d0c9164571c19fa5950edd3066415b12c09
Ab-Ezekiel <akpanabraham1000@gmail.com>
2025-10-30 12:12:24 +0100

    initial commit

---
+++ school/students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'students'
+
+urlpatterns = [
+    # TODO: Add path for the student list view
+]
---
+++ school/students/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# from .models import Student
+
+# TODO: Create a view function to display all students.
+# Use Student.objects.all() to fetch data.
+# Pass it into a template (student_list.html).
